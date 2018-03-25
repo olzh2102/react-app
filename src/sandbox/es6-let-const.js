4ffd80af1f7f106bbe810b34f933b5deaaea63ae
olzhas2102 <olzhas2102@yahoo.com>
2018-03-25 17:08:53 +0200

    Initial commit - skeleton for next app

--- src/sandbox/es6-let-const.js
+++
@@ -1,9 +0,0 @@
-var nameVar = 'Rachel';
-console.log('nameVar', nameVar);
-
-let nameLet = 'Kate';
-nameLet = 'Emma';
-console.log('nameLet', nameLet);
-
-const nameConst = 'Alice';
-console.log('nameConst', nameConst);
